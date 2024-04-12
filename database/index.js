5225b94d1fd5893e6fa14f95a6e64615a2cea2d7
AmineTouati <128828801+BlackGhost789@users.noreply.github.com>
2024-04-12 18:37:45 +0200

    Update index.js

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config()
 
-mongo.connect(process.env.MONGO_URL)
+mongo.connect(process.env.MONGO_URI)
 .then(()=> {console.log('connected to database')})
 .catch((err)=> {console.log(`error ${err}`)})
 
